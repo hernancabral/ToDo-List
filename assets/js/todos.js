a7b96238c854e11b80fefac734cd17f8249c5540
hernancabral <42773790+hernancabral@users.noreply.github.com>
2019-01-28 10:03:20 -0300

    Update 28/1
    Se agrega selector de idioma.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,7 +5,7 @@ $("ul").on("click", "li", function(){
 });
 
 //Eliminar los items con animacion
-$("ul").on("click", "span", function(event){
+$("ul").on("click", "li span", function(event){
     $(this).parent().fadeOut("1000", function(){
         $(this).remove();
     });
@@ -18,10 +18,45 @@ $("input[type='text']").keypress(function(event){
         var todoText = $(this).val();
         $(this).val("");
         //Se crea un li agregandolo al ul
-        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+        $("ul").append("<li><span class='bin'><i class='fa fa-trash'></i></span> " + todoText + "</li>");
     }
 });
 
 $(".fa-plus").click(function(){
     $("input[type='text']").fadeToggle();
+});
+
+//Diccionarios de idioma
+var spaDct = {
+    lng: "Español",
+    title: "To-Do List",
+    placeHolder: "Add New Todo",
+};
+
+var engDct = {
+    lng: "English",
+    title: "Tareas",
+    placeHolder: "Agregar Tarea",
+};
+
+//Traduccion
+function getDct(lng) {
+    switch(lng) {
+        case "English":
+            return spaDct;
+        case "Español":
+            return engDct;
+    };
+}
+
+function translate(lng) {
+    var idm = getDct(lng);
+    $("#languageBtn").text(idm.lng);
+    $("#title").text(idm.title);
+    $("input").val(idm.placeHolder);
+}
+
+$("#stripe").on("click", "#languageBtn",function(event){
+    translate($("#languageBtn").text());
+    event.stopPropagation();
 });
